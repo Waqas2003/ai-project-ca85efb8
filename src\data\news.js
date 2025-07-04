06077c4abb9522997aeb2b5dc8c2a6d05cd4a002
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:33:03 +0500

    Add src\data\news.js

---
+++ src\data\news.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, title: 'Pakistan wins against India in T20 World Cup' },
+  { id: 2, title: 'Babar Azam becomes top-ranked batsman in ICC rankings' },
+  { id: 3, title: 'Pakistan Cricket Board announces new team management' },
+  // ...
+];
