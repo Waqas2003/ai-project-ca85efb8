8f59ed8b541a10926fabf132a67efb9f9e89f795
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:33:06 +0500

    Add src\data\schedule.js

---
+++ src\data\schedule.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, date: '2023-02-10', opponent: 'India' },
+  { id: 2, date: '2023-02-15', opponent: 'Australia' },
+  { id: 3, date: '2023-02-20', opponent: 'England' },
+  // ...
+];
